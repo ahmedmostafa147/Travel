995c92c633c8483c423b9e30b805fc18bfb4d2a4
Ahmed Mostafa <42010147.Ahmed@acu.edu.eg>
2024-01-14 12:18:14 +0200

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   initializeDateFormatting().then((_) => runApp(DevicePreview(
-        enabled: true,
+        enabled: false,
         builder: (context) => const Travel(), // Wrap your app
       )));
 }
